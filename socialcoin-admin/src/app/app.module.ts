bb387ec407eef020685cacee161fc29535c13975
Eduardo Thomaz de Carvalho <ethomaz@id.uff.br>
2018-07-31 11:48:37 +0100

    Auto stash before merge of "prototype-angular-material" and "origin/prototype-angular-material"

--- socialcoin-admin/src/app/app.module.ts
+++ socialcoin-admin/src/app/app.module.ts
@@ -4,27 +4,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { RegisterParticipantComponent } from './register-participant/register-participant.component';
-import { RegisterActivityComponent } from './register-activity/register-activity.component';
-import { RegisterNeighborTeamComponent } from './register-neighbor-team/register-neighbor-team.component';
-import { AssignActivityNeighborTeamComponent } from './assign-activity-neighbor-team/assign-activity-neighbor-team.component';
-import { NtMenuComponent } from './nt-menu/nt-menu.component';
-import { ShowActivityListComponent } from './show-activity-list/show-activity-list.component';
-import { ShowActivityDetailComponent } from './show-activity-detail/show-activity-detail.component';
-import { AssignActivityParticipantComponent } from './assign-activity-participant/assign-activity-participant.component';
-import { CheckCompletedActivityComponent } from './check-completed-activity/check-completed-activity.component';
-import { AcceptRejectCompletedActivityComponent } from './accept-reject-completed-activity/accept-reject-completed-activity.component';
-import { AssignParticipantNeighborTeamComponent } from './assign-participant-neighbor-team/assign-participant-neighbor-team.component';
+import { RegisterParticipantComponent } from './ca/register-participant/register-participant.component';
+import { RegisterActivityComponent } from './ca/register-activity/register-activity.component';
+import { RegisterNeighborTeamComponent } from './ca/register-neighbor-team/register-neighbor-team.component';
+import { AssignActivityNeighborTeamComponent } from './ca/assign-activity-neighbor-team/assign-activity-neighbor-team.component';
+import { MenuComponent as NtMenuComponent} from './nt/menu/menu.component';
+import { ShowActivityListComponent } from './nt/show-activity-list/show-activity-list.component';
+import { ShowActivityDetailComponent } from './nt/show-activity-detail/show-activity-detail.component';
+import { AssignActivityParticipantComponent } from './nt/assign-activity-participant/assign-activity-participant.component';
+import { CheckCompletedActivityComponent } from './nt/check-completed-activity/check-completed-activity.component';
+import { AcceptRejectCompletedActivityComponent } from './nt/accept-reject-completed-activity/accept-reject-completed-activity.component';
+import { AssignParticipantNeighborTeamComponent } from './ca/assign-participant-neighbor-team/assign-participant-neighbor-team.component';
+import { MenuComponent as CaMenuComponent } from './ca/menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
     HeaderComponent,
     FooterComponent,
     RegisterParticipantComponent,
@@ -37,7 +36,8 @@ import { AssignParticipantNeighborTeamComponent } from './assign-participant-nei
     AssignActivityParticipantComponent,
     CheckCompletedActivityComponent,
     AcceptRejectCompletedActivityComponent,
-    AssignParticipantNeighborTeamComponent
+    AssignParticipantNeighborTeamComponent,
+    CaMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ socialcoin-admin/src/app/ca/menu/menu.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'sca-ca-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  @Output() closeSidenav = new EventEmitter<void>();
+
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
+
+  onClose() {
+    this.closeSidenav.emit();
+  }
+
+}
